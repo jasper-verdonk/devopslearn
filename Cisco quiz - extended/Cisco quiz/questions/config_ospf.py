5ea1a8750f883588f66eefb011aa7e48f04fbdbb
jasper-verdonk <info@jasperverdonk.nl>
2025-08-25 08:57:01 +0200

    Delete Cisco quiz - extended directory

--- Cisco quiz - extended/Cisco quiz/questions/config_ospf.py
+++
@@ -1,6 +0,0 @@
-questions = {
-    "Initialize the OSFP process 100": "router ospf 100",
-    "Under OSFP process 100 hierarchy, define RID 192.168.1.1": "router-id 192.168.1.1",
-    "Under OSFP process 100 hierarchy, enable OSFP on all interfaces with one statement for area 1234": "network 0.0.0.0 255.255.255.255 area 1234",
-    "Under OSFP process 100 hierarchy, enable OSFP on interface with IP 192.168.10.10 very specific for area 1234": "network 192.168.10.10 0.0.0.0 area 1234",
-}

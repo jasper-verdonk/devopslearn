15c82593b06a7934bab972e94c46bcc3f64ca7de
jasper-verdonk <info@jasperverdonk.nl>
2025-08-25 08:57:11 +0200

    Delete Cisco quiz directory

--- Cisco quiz/Cisco quiz/questions/config_eigrp.py
+++
@@ -1,7 +0,0 @@
-questions = {
-    "Initialize the EIGRP process for ASN 65000": "router eigrp 65000",
-    "Initialize the EIGRP named process EIGRP-NAMED": "router eigrp EIGRP-NAMED",
-    "Under EIGRP process ASN 65000 hierarchy, define RID 192.168.1.1": "eigrp router-id 192.168.1.1",
-    "Under EIGRP named process define ANS 100 ipv4 unicast": "address-family ipv4 unicast autonomous-system 100",
-    
-}
